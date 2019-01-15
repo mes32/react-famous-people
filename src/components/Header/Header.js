aacd463852f0eaaeb1b7b6dd5ef5c9f9f916bf61
mes32 <stockman.mike@gmail.com>
2019-01-15 15:33:25 -0600

    Create a Header Component to show the app name in a colored banner across the top of the page.

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <header>
+                    React Famous People
+                </header>
+            </div>
+        );
+    }
+}
+
+export default Header;
